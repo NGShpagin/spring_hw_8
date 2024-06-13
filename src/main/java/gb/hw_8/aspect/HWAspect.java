0f96f76450a54f2221c871553643ae6e3e36f2e5
Nikolay <shpagin9393@mail.ru>
2024-06-13 10:21:19 +0300

    merge Aspects

--- src/main/java/gb/hw_8/aspect/HWAspect.java
+++ src/main/java/gb/hw_8/aspect/HWAspect.java
@@ -3,19 +3,42 @@ package gb.hw_8.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @Aspect
 @Component
-public class RecoverExceptionAspect {
+public class HWAspect {
+
+    private Instant timeStart;
+
+    @Pointcut("within(@gb.hw_8.aspect.Timer *)")
+    public void beansAnnotatedWith() {
+
+    }
+
+    @Pointcut("@annotation(gb.hw_8.aspect.Timer)")
+    public void methodsAnnotatedWith() {
+
+    }
+
+    @Before("methodsAnnotatedWith() || beansAnnotatedWith()")
+    public void beforeTimer(JoinPoint joinPoint) {
+        timeStart = Instant.now();
+    }
+
+    @After("methodsAnnotatedWith() || beansAnnotatedWith()")
+    public void afterTimer(JoinPoint joinPoint) {
+        String completionTime = String.valueOf(Duration.between(Instant.now(), timeStart));
+        log.info("{} - {} #{}", joinPoint.getTarget().getClass(), joinPoint.getSignature().getName(), completionTime);
+    }
 
     @Pointcut("@annotation(gb.hw_8.aspect.RecoverException)")
     public void recoverExcept() {
